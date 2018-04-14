3aa2c0f7d00e9f95a7360fad1cb5092bb212d2ba
Simon <sisihamilton@hotmail.com>
2018-04-14 12:04:53 -0400

    css grid squares

--- app/scripts.js
+++ app/scripts.js
@@ -2,16 +2,9 @@ let getMap = document.getElementById("map")
 let mapBtn = document.getElementById("map-button")
 
 showMap = () => {
-    console.log("map")
     getMap.style.display = "block"
     mapBtn.style.display = "none"
 }
 
-// btnHover = () => {
-//     console.log("hover")
-//     mapBtn.style.backgroundColor = "white"
-// }
-
 mapBtn.addEventListener('click',showMap )
-// mapBtn.addEventListener('mouseenter', btnHover)
 
